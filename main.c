46512f0d4382c95734fcd9f3c70d09a791551237
John Cobb <john@cphandheld.com>
2014-10-24 14:41:44 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 11, 2014
+ *      Author: jcobb
+ */
+
+#define F_CPU	8000000
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB |= _BV(PB5);
+
+	while (1)
+	{
+		// toggle led
+		PORTB ^= _BV(PB5);
+		_delay_ms(1000);
+
+	}
+}
